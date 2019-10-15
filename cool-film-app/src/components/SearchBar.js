4f7a2037a618a6ad375482994ec6d048f8fabbee
Bendik <bendikbrunvoll@gmail.com>
2019-10-15 13:15:07 +0200

    Make a search bar. This search bar does not do anything interesting yet. Ref #5

---
+++ cool-film-app/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+               <form method={"get"}>
+                    <input type={"text"} name={"search"} placeholder={"What are you looking for?"}/>
+                    <input type={"submit"}/>
+               </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
