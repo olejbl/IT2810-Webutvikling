d50a9c25992738baf7ee68776b8722b00f0ba8f0
henrihod <henrikstabekk@gmail.com>
2019-10-22 13:44:12 +0200

    Fungerende mongodb og API spørringer til mongodb
    brukt denne tutorial til å sette opp: https://www.youtube.com/watch?v=vjf774RKrLc

---
+++ cool-film-app/API/posts/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post')
+
+//HENTER UT ALT AV POSTS
+router.get('/', async (req, res) => {
+try{
+const posts = await Post.find();
+res.json(posts)
+}
+catch(err){
+  res.json({message: err});
+}
+});
+
+router.get('/spesific', (req, res) => {
+res.send('We are on Spesific');
+});
+
+
+// SENDER INN POSTS
+router.post('/', (req,res) => {
+const post = new Post({
+  title: req.body.title,
+  description: req.body.description,
+  date: req.body.date
+
+})
+post.save()
+.then(data => {
+res.json(data);
+})
+.catch(err =>{
+  res.json({message: err});
+})
+
+});
+
+module.exports = router
