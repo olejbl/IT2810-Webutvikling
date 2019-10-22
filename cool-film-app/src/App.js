7caf9dddb4fe9646c9046b8da925b5ba46b82205
henrihod <henrikstabekk@gmail.com>
2019-10-22 10:31:52 +0200

    installerer package.json

--- cool-film-app/src/App.js
+++ cool-film-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Provider from "react-redux/lib/components/Provider";
 import SearchBar from "./components/SearchBar";
 import ListView from "./components/list-view.js";
+import Database from "./components/database-connect.js";
 import  "./components/list-view.css";
 import store from "./store";
 
@@ -13,6 +14,7 @@ function App() {
               <header className="App-header">
               <ListView></ListView>
               </header>
+                  <Database></Database>
               <SearchBar/>
           </div>
       </Provider>
---
+++ cool-film-app/src/components/database-connect.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+//mongoose.connect('mongodb://it2810-50.idi.ntnu.no:27017/test');
+class Database extends Component {
+    render() {
+        return (
+            <div>
+            <h1> test </h1>
+            </div>
+        );
+    }
+}
+
+export default Database;
