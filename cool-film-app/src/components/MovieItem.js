e770fc51393d2a18c253c25861e5c61b4609d4cc
Bendik <bendikbrunvoll@gmail.com>
2019-10-22 12:36:57 +0200

    Sentrere innholdet i film-elementene og få Ip-Man til å slutte å være rar #11 #12

--- cool-film-app/src/components/MovieItem.js
+++ cool-film-app/src/components/MovieItem.js
@@ -2,11 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  margin: 18px;
+  margin-right: 2vw;
+  margin-left: 2vw;
+  margin-top: 3vh;
+  max-width: 150px;
 `
 
 const Poster = styled.img`
   width: 150px;
+  left: 50%;
+  height: 225px;
 `
 
 const Text = styled.div`
--- cool-film-app/src/components/MoviePopoutContent.js
+++ cool-film-app/src/components/MoviePopoutContent.js
@@ -2,36 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  
   background-color: white;
-  border-radius: 1%;
-  padding: 4%;
+  border-radius: 8px;
+  padding: 16px;
   box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302), 0 1px 3px 1px rgba(60,64,67,0.14);
-  width: 20vmin;
-  margin: auto;
-  
-`;
+`
 
 const Title = styled.div`
   padding: 5px;
   font-size: 12px;
-  font-weight: bold;
-  margin: auto;
-  text-align: center;
-`;
+`
 
 const Poster = styled.img`
-  width: 100%;
-`;
+  width: 150px;
+`
 
 const Text = styled.div`
   font-size: 12px;
   margin-bottom: 4px;
-`;
+`
 
 const TextContainer = styled.div`
-  width: 100%;
-`;
+  min-width: 150px;
+  max-width: 230px;
+`
 
 export default function MoviePopoutContent ({
   title,
