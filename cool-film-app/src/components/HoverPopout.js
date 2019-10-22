155aae7c65dedd9fdfc156fb615d8d9be4a696e4
Bendik <bendikbrunvoll@gmail.com>
2019-10-22 11:52:25 +0200

    Make the font "Arial" and center the text #11

--- cool-film-app/src/components/HoverPopout.js
+++ cool-film-app/src/components/HoverPopout.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 
 const PopoutContainer = styled.div`
   position: absolute;
-`
+  margin: auto;
+  -moz-box-shadow: 0 0 3px #ccc;
+  -webkit-box-shadow: 0 0 3px #ccc;
+  box-shadow: 0 0 3px #ccc;
+  
+`;
 
-export default function HoverPopout ({ children, popout: PopoutContent }) {
-  const [ isHovering, setIsHovering ] = useState(false);
+export default function HoverPopout({children, popout: PopoutContent}) {
+    const [isHovering, setIsHovering] = useState(false);
 
-  return (
-    <>
-      <div
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-      >
-        {children}
-      </div>
-      {isHovering ? (
-        <PopoutContainer>
-          {PopoutContent}
-        </PopoutContainer>
-        ) : null}
-    </>
-  );
+    return (
+        <>
+            <div
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
+            >
+                {children}
+            </div>
+            {isHovering ? (
+                <PopoutContainer>
+                    {PopoutContent}
+                </PopoutContainer>
+            ) : null}
+        </>
+    );
 }
--- cool-film-app/src/components/MoviePopoutContent.js
+++ cool-film-app/src/components/MoviePopoutContent.js
@@ -2,30 +2,36 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
+  
   background-color: white;
-  border-radius: 8px;
-  padding: 16px;
+  border-radius: 1%;
+  padding: 4%;
   box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302), 0 1px 3px 1px rgba(60,64,67,0.14);
-`
+  width: 20vmin;
+  margin: auto;
+  
+`;
 
 const Title = styled.div`
   padding: 5px;
   font-size: 12px;
-`
+  font-weight: bold;
+  margin: auto;
+  text-align: center;
+`;
 
 const Poster = styled.img`
-  width: 150px;
-`
+  width: 100%;
+`;
 
 const Text = styled.div`
   font-size: 12px;
   margin-bottom: 4px;
-`
+`;
 
 const TextContainer = styled.div`
-  min-width: 150px;
-  max-width: 230px;
-`
+  width: 100%;
+`;
 
 export default function MoviePopoutContent ({
   title,
