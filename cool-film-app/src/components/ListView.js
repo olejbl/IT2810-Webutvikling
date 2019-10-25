b68dd5f7addfef5808e4dd6e39106bb1348aa245
Bendik <bendikbrunvoll@gmail.com>
2019-10-25 10:46:47 +0200

    Make the ListView only render 14 movies per page ref #15

--- cool-film-app/src/components/ListView.js
+++ cool-film-app/src/components/ListView.js
@@ -4,61 +4,65 @@ import MovieItem from './MovieItem'
 import HoverPopout from './HoverPopout'
 import MoviePopoutContent from './MoviePopoutContent'
 import {Component} from 'react'
+import ReactPaginate from 'react-paginate';
 
-const Container = styled.div`
+const MovieListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 `;
 
-const makeHttpRequestWithPage = async pageNumber => {
-    let response = await fetch(`https://reqres.in/api/users?page=${pageNumber}`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-    });
-
-    const data = await response.json();
-
-    this.setState({
-        users: data.data,
-        total: data.total,
-        per_page: data.per_page,
-        current_page: data.page,
-    });
-};
-
 class ListView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: this.props.movies,
+            per_page: 14,
+            current_page: 1
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        this.setState({
+            currentPage: Number(event.target.id)
+        });
+    }
+
     render() {
+        const { movies, per_page, current_page } = this.state;
+        const finalMovieOnPageIndex = current_page * per_page;
+        const firstMovieOnPageIndex = finalMovieOnPageIndex - per_page;
+        const currentMovies = this.props.movies.slice(firstMovieOnPageIndex, finalMovieOnPageIndex);
         return (
             <div>
-                <Container>
-                  {this.props.movies.map((movie, index) =>
-                      <div key={index}>
-                      <HoverPopout
-                    popout={
-                        <MoviePopoutContent
-                            title={movie.title}
-                            posterUrl={movie.posterUrl}
-                            year={movie.year}
-                            rating={movie.rating}
-                            plot={movie.plot}
-                        />
-                    }>
-                    <MovieItem
-                    title={movie.title}
-                    posterUrl={movie.posterUrl}
-                    year={movie.year}
-                    />
-                    </HoverPopout></div>)
-
-                  }
-                </Container>
+                <MovieListContainer>
+                    {currentMovies.map((movie, index) =>
+                        <div key={index}>
+                            <HoverPopout
+                                popout={
+                                    <MoviePopoutContent
+                                        title={movie.title}
+                                        posterUrl={movie.posterUrl}
+                                        year={movie.year}
+                                        rating={movie.rating}
+                                        plot={movie.plot}
+                                    />
+                                }>
+                                <MovieItem
+                                    title={movie.title}
+                                    posterUrl={movie.posterUrl}
+                                    year={movie.year}
+                                />
+                            </HoverPopout></div>
+                    )}
+                </MovieListContainer>
             </div>
         );
     }
+
+
 }
 
 export default ListView;
